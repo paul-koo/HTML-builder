b149b30be52ef358c6bd7b449bf2a489035bf5c0
paul-koo <pavelkoltun999@gmail.com>
2024-01-23 15:02:44 +0300

    feat: compelet first task with stream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,12 @@
 const fs = require('fs'); //подключение модуля file system (require команда подключения модуля)
 const path = require('path'); //подключение модуля path
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {  //первый параметр путь к файла, второй параметр кодировка, третий параметр функция обратного вызова
-    if (err) throw err;
-    console.log(data);
+// const stream = require('node:stream'); //подключение модуля stream, для работы с потоками
+const streamRead = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+streamRead.on('data', (chunk) => {
+    console.log(chunk);
 })
+
+// fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {  //первый параметр путь к файла, второй параметр кодировка, третий параметр функция обратного вызова
+//     if (err) throw err;
+//     console.log(data);
+// })
