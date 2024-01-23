c9fad67c479162e0ae9163a024b50f96d755b567
paul-koo <pavelkoltun999@gmail.com>
2024-01-23 22:51:04 +0300

    feat: second task completed

---
+++ 02-write-file/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+process.stdout.write("Введите текст для записи:\n");
+process.stdin.on('data', (data) => {
+    if (data.toString().trim() === 'exit') {
+        process.exit();
+    }
+    writeStream.write(data.toString().trim() + '\n');
+})
+
+process.on('exit', () => {
+    console.log('Спасибо запись в файл программы завершена!');
+})
